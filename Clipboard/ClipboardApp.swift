e2e3701d0ef4f99b200b741b09c1ac37fcb9c938
Aayush Giri <aayushgiri1234@gmail.com>
2025-10-18 16:40:18 +0530

    Initial Commit

---
+++ Clipboard/ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardApp.swift
+//  Clipboard
+//
+//  Created by Aayush Giri on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
